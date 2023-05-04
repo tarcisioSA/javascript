544e8fba962fe609b434f8ab1a01e04cf17d97c6
Tarcisio Sousa Almeida <126097113+tarcisioSA@users.noreply.github.com>
2023-05-03 23:17:45 -0300

    update

---
+++ bootcampDio/pokedex/assents/js/main.js
@@ -0,0 +1,36 @@
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+const limit = 5
+const offset = 0;
+
+    function loadPokemonsItens(offset, limit){
+
+            pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+            const newHtml = pokemons.map((pokemon) => `
+                    <li class="pokemon ${pokemon.type}">
+                    <span class="number">#${pokemon.number}</span>
+                    <span class="name">${pokemon.name}</span>
+                    
+                    <div class="detail">
+                        <ol class="types">
+                            ${pokemon.types.map((type) => `<li class="type ${type} ">${type}</li>` ).join('')}
+                
+                        </ol>
+                        <img src="${pokemon.photo}" alt="${pokemon.name}">
+                    </div>
+                </li>`).join('')
+
+
+            pokemonList.innerHTML += newHtml
+    
+        })
+    }
+    
+    loadPokemonsItens(offset, limit)
+
+    loadMoreButton.addEventListener('click', () => {
+        offset += limit
+        loadPokemonsItens(offset, limit)
+    })
+
+ 
---
+++ teste.js
@@ -0,0 +1,6 @@
+const lista = ['1', '2', '4', '5', '6', '7', '8', '9', '10']
+for (let i = 0; i < 9; i++) {
+    const numero = lista[i]
+    console.log(numero)
+    
+    }
