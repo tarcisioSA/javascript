3171d6bbc5e77e5a3a08423052981af27054d737
Tarcisio Sousa Almeida <126097113+tarcisioSA@users.noreply.github.com>
2023-05-09 11:18:37 -0300

    new

--- teste.js
+++ teste.js
@@ -1,6 +1,31 @@
-const lista = ['1', '2', '4', '5', '6', '7', '8', '9', '10']
-for (let i = 0; i < 9; i++) {
-    const numero = lista[i]
-    console.log(numero)
-    
-    }
+const numPedidos = parseInt(2);
+let totalCalorias = 0;
+let valorTotal = 0;
+
+for (let i = 1; i <= numPedidos; i++) {
+  let prato = '';
+  let calorias = parseInt();
+  let ehVegano = 's'.toLowerCase() === 's';
+  
+  if (ehVegano) {
+    if(i === 1){
+      prato = 'Hamburguer de lentilha';
+      calorias = 300;
+      let vegano = 'Vegano';
+      
+  }
+  if(i === 2){
+      prato = 'Salada de frutas';
+      calorias = 60;
+      let vegano = 'Vegano';
+      
+  }
+  } 
+
+
+  const tipoPrato = ehVegano ? 'Vegano' : 'Não-vegano';
+  console.log(`Pedido ${i}: ${prato} (${tipoPrato}) - ${calorias} calorias`);
+}
+
+
+
