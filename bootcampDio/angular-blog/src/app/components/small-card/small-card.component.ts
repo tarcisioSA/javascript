f7f92d5235799b0bdfbaa612b358190f938cb36a
Tarcisio Sousa Almeida <126097113+tarcisioSA@users.noreply.github.com>
2023-05-21 20:26:50 -0300

    new

--- bootcampDio/angular-blog/src/app/components/small-card/small-card.component.ts
+++ bootcampDio/angular-blog/src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./small-card.component.css']
 })
 export class SmallCardComponent implements OnInit {
+  @Input()
+  photo:string = ''
+  @Input()
+  cardTitle:string = ''
+  @Input()
+  cardDescription:string = ''
+  @Input()
+  linkSites:string = ''
 
   constructor() { }
 
